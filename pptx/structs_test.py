dbeb3632a8970013688e490cc18622687ee6d524
Marc Cataford <mcat@riseup.net>
2024-01-17 13:40:59 -0500

    feat(text-support): ensure bold+italics text properties are extracted

--- pptx/structs_test.py
+++ pptx/structs_test.py
@@ -34,16 +34,24 @@ def test_extracts_no_shapes_from_empty_slide(get_sample):
 
 def test_extracts_shape_from_nonempty_slide(get_sample):
     """A slide with exactly one element has exactly one shape."""
-    sample = get_sample("single_text_slide", 0)
+    sample = get_sample("text_samples", 0)
 
     slide = Slide.from_xml(sample)
 
     assert len(slide.shapes) == 1
 
 
+def test_extracts_multiple_shapes_from_slide(get_sample):
+    sample = get_sample("text_samples", 1)
+
+    slide = Slide.from_xml(sample)
+
+    assert len(slide.shapes) == 3
+
+
 def test_extracts_paragraphs_from_shape_with_text(get_sample):
     """A slide containing a text box with one block of text has one paragraph."""
-    sample = get_sample("single_text_slide", 0)
+    sample = get_sample("text_samples", 0)
 
     slide = Slide.from_xml(sample)
     shape = slide.shapes[0]
@@ -54,7 +62,7 @@ def test_extracts_paragraphs_from_shape_with_text(get_sample):
 
 def test_extracts_text_runs_from_shape_with_text(get_sample, snapshot):
     """A paragraph is made of runs."""
-    sample = get_sample("single_text_slide", 0)
+    sample = get_sample("text_samples", 0)
 
     slide = Slide.from_xml(sample)
     shape = slide.shapes[0]
@@ -68,3 +76,22 @@ def test_extracts_text_runs_from_shape_with_text(get_sample, snapshot):
 
     assert run.text == snapshot
     assert run.props == snapshot
+
+
+@pytest.mark.parametrize(
+    "run_text,expected_props",
+    [["boldness", {"b": "1"}], ["italics", {"i": "1"}]],
+    ids=["bold", "italics"],
+)
+def test_extracts_text_formatting_properties_from_shape_with_text(
+    get_sample, snapshot, run_text, expected_props
+):
+    sample = get_sample("text_samples", 2)
+
+    paragraph = Slide.from_xml(sample).shapes[0].text.paragraphs[0]
+    runs = paragraph.runs
+
+    selected_run = [run for run in runs if run.text == run_text][0]
+
+    for prop in expected_props:
+        assert expected_props[prop] == selected_run.props[prop]
